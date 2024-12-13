cdee199c8b33804db3a396df298326f72416e6b5
kish0167 <simerkish@gmail.com>
2024-12-13 14:31:29 +0300

    root namespace & project installer

---
+++ Assets/Scripts/Installers/ProjectInstaller.cs
@@ -0,0 +1,16 @@
+using Zenject;
+
+namespace Installers
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        #region Public methods
+
+        public override void InstallBindings()
+        {
+            
+        }
+
+        #endregion
+    }
+}
