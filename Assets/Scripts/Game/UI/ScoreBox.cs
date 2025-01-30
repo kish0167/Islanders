c7e7ead583aeb063151edc6dbf69625a360a2d24
kish <simerkish@gmail.com>
2025-01-30 16:21:20 +0300

    player score, finally

---
+++ Assets/Scripts/Game/UI/ScoreBox.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Islanders.Game.UI
+{
+    public class ScoreLabel : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _scoreFloorLabel;
+        [SerializeField] private TMP_Text _scoreLabel;
+        [SerializeField] private TMP_Text _scoreCeilingLabel;
+        
+        
+        public void UpdateLabels(int scoreFloor, int score, int scoreCeiling)
+        {
+            _scoreFloorLabel.text = scoreFloor.ToString();
+            _scoreLabel.text = score.ToString();
+            _scoreCeilingLabel.text = scoreCeiling.ToString();
+        }
+    }
+}
